ad96a9c2982dc70f11496641000e72e07a89e2d9
Oleksandr Kozachenko <olexandr.kozachenko@gmail.com>
2023-04-13 23:04:01 +0300

    Added Orders

--- administrator/views.py
+++ administrator/views.py
@@ -12,8 +12,8 @@ from rest_framework.views import APIView
 
 from common.authentication import JWTAuthentication
 from common.serializers import UserSerializer
-from core.models import User, Product
-from .serializers import ProductSerializer
+from core.models import User, Product, Link, Order
+from .serializers import ProductSerializer, LinkSerializer, OrderSerializer
 
 
 class AmbassadorAPIView(APIView):
@@ -54,3 +54,23 @@ class ProductGenericAPIView(
 
     def delete(self, request, pk=None):
         return self.destroy(request, pk)
+
+
+class LinkAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk=None):
+        links = Link.objects.filter(user_id=pk)
+        serializer = LinkSerializer(links, many=True)
+        return Response(serializer.data)
+
+
+class OrderAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        orders = Order.objects.filter(complete=True)
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
